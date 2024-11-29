d7c41f54540b6db7df7fb95e3e3c3ba37a023848
bhumilalwani <lalwanibhumi91@gmail.com>
2024-11-29 12:11:30 +0530

    updated05

--- src/app/components/FooterLastestStrip.jsx
+++ src/app/components/FooterLastestStrip.jsx
@@ -12,25 +12,25 @@ const FooterLastestStrip = () => {
                 <img src="https://s.yimg.com/fz/api/res/1.2/ILro_VoCX8RmD6bm6gzLuw--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpdDtoPTI0MDtxPTgwO3c9MjQw/https://s.yimg.com/zb/imgv1/9736e341-9507-395b-a22e-50847071d65c/t_500x300" alt="Visa" className="h-10"/>
             </div>
             <div className="image-wrapper">
-                <img src="https://s.yimg.com/fz/api/res/1.2/ILro_VoCX8RmD6bm6gzLuw--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpdDtoPTI0MDtxPTgwO3c9MjQw/https://s.yimg.com/zb/imgv1/9736e341-9507-395b-a22e-50847071d65c/t_500x300" alt="Mastercard" className="h-10"/>
+                <img src="http://127.0.0.1:5500/angara/img/mastercard.svg" alt="Mastercard" className="h-10"/>
             </div>
             <div className="image-wrapper">
-                <img src="img/amEx.png" alt="AmEx" className="h-10"/>
+                <img src="http://127.0.0.1:5500/angara/img/amEx.png" alt="AmEx" className="h-10"/>
             </div>
             <div className="image-wrapper">
-                <img src="img/affirm.svg" alt="Affirm" className="h-10"/>
+                <img src="http://127.0.0.1:5500/angara/img/affirm.svg" alt="Affirm" className="h-10"/>
             </div>
             <div className="image-wrapper">
-                <img src="img/amazon-pay.svg" alt="Amazon Pay" className="h-10"/>
+                <img src="http://127.0.0.1:5500/angara/img/amazon-pay.svg" alt="Amazon Pay" className="h-10"/>
             </div>
             <div className="image-wrapper">
-                <img src="img/norton-secured.svg" alt="Norton Secured" className="h-10"/>
+                <img src="http://127.0.0.1:5500/angara/img/amazon-pay.svg" alt="Norton Secured" className="h-10"/>
             </div>
             <div className="image-wrapper">
-                <img src="img/accredited-business.svg" alt="Accredited Business" className="h-10"/>
+                <img src="http://127.0.0.1:5500/angara/img/norton-secured.svg" alt="Accredited Business" className="h-10"/>
             </div>
             <div className="image-wrapper">
-                <img src="img/bizrate.png" alt="Bizrate" className="h-10"/>
+                <img src="http://127.0.0.1:5500/angara/img/accredited-business.svg" alt="Bizrate" className="h-10"/>
             </div>
         </div>
     </div>
